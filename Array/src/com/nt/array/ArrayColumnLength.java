089fe3370c10e4d2b34e70e8d49172392e4eb6f3
User <User@192.168.0.10>
2020-04-23 01:05:12 +0530

    arraylength

---
+++ Array/src/com/nt/array/ArrayColumnLength.java
@@ -0,0 +1,21 @@
+package com.nt.array;
+
+public class ArrayColumnLength {
+	
+	public static void main(String[] args) {
+		int a[][]=new int[3][9];
+		System.out.println(a.length);
+		System.out.println("column length is::"+ a[0].length);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
